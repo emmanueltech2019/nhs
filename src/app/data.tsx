8234a38cd411fcf955537f03498d4226227b5b3a
EMMANUEL LUCKY <emmanueltech2019@gmail.com>
2024-06-22 16:32:29 +0100

    first commit

--- src/app/data.tsx
+++ src/app/data.tsx
@@ -13,7 +13,6 @@ const data:Data ={
     domain: "newyorkherniaspecialists.com",
     logo: 'https://res.cloudinary.com/wise-solution-inc/image/upload/v1719069492/1Asset_47_zw1sgi.png',
     src:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d387191.0360990812!2d-74.30933360194288!3d40.697539958252754!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sng!4v1719069448860!5m2!1sen!2sng"
-
 }
 
 
